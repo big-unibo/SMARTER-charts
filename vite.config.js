4179b915297cb04aa47f522f232d6b3b6af6c3f7
Alex Baiardi <alex.baiardi99@gmail.com>
2025-11-20 15:59:03 +0100

    fix: set build output directory

--- vite.config.js
+++ vite.config.js
@@ -17,18 +17,20 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
     lib: {
       entry: path.resolve(__dirname, 'src/main.js'),
       name: 'smarter-charts',
       fileName: (format) => `smarter-charts.${format}.js`
-    }
-  },
-  rollupOptions: {
-    external: ['vue'], // Exclude vue from the bundle
-    output: {
-      globals: {
-        vue: 'Vue'
+    },
+    rollupOptions: {
+      external: ['vue'], // Exclude vue from the bundle
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
       }
     }
-  }
+  },
 })
