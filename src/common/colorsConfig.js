291af70235626b33387e24103887acd3dab24a5d
Alex Baiardi <alex.baiardi99@gmail.com>
2026-06-17 16:18:40 +0200

    fix: loose equality

--- src/common/colorsConfig.js
+++ src/common/colorsConfig.js
@@ -1,16 +1,16 @@
 
 export const binningColorConfig = function (value) {
-  if (value == 6) {
+  if (value === 6) {
     return 'rgb(5, 48, 97)';
-  } else if (value == 5) {
+  } else if (value === 5) {
     return 'rgb(51, 124, 183)';
-  } else if (value == 4) {
+  } else if (value === 4) {
     return 'rgb(143, 194, 221)';
-  } else if (value == 3) {
+  } else if (value === 3) {
     return 'rgb(241, 163, 133)'
-  } else if (value == 2) {
+  } else if (value === 2) {
     return 'rgb(195, 61, 61)';
-  } else if (value == 1) {
+  } else if (value === 1) {
     return 'rgb(140, 13, 37)';
   } else return 'rgb(255, 255, 255)'
 };
